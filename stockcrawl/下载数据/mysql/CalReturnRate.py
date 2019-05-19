7f46fd54a2dea4ff82ad0df6c5992c492fefb54f
136397089 <136397089@163.com>
2019-05-19 10:38:45 +0800

    '备份工具'

--- stockcrawl/下载数据/mysql/CalReturnRate.py
+++ stockcrawl/下载数据/mysql/CalReturnRate.py
@@ -5,5 +5,8 @@ import pandas as pd
 import numpy as np
 import math
 
+def GetAllCloseDate(tablename,NumType,engine):
+    sql = "select close from " + tablename+' ; '
+    data = pd.read_sql_query(sql, engine)
 
 
